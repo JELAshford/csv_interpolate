d40d48f493fdf56e9ce2623289d10e818b218e0d
James Ashford <ashfordj@rocketmail.com>
2024-09-18 15:20:14 +0100

    Formatting changes

--- src/csv_interpolate/cli.py
+++ src/csv_interpolate/cli.py
@@ -2,27 +2,25 @@ from .interp_methods import interpolate_at_nans, interpolate_convolve
 import numpy as np
 import click
 
+
 INTERP_METHODS = {
     "convolve": interpolate_convolve,
     "iterated": interpolate_at_nans,
 }
-METHODS_STRING = f"`{'`, `'.join(list(INTERP_METHODS.keys()))}`"
+METHODS = f"`{'`, `'.join(list(INTERP_METHODS.keys()))}`"
 
 
 @click.command()
 @click.option("--csv_path", help="Path to file you want to interpolate")
 @click.option(
-    "--method",
-    default="convolve",
-    help=f"Algorithm to use, one of: {METHODS_STRING}",
+    "--method", default="convolve", help=f"Algorithm to use, one of: {METHODS}"
 )
 @click.option(
     "--save_path", default="output.csv", help="Path to save interpolated matrix to"
 )
 def interpolate(csv_path: str, method: str = "iterated", save_path: str = "output.csv"):
-    """Interpolate a 2D matrix, provided as a `.csv` file, so all `nan` values are replaced
-    with the average of their neighbours.
-    """
+    """Interpolate a 2D matrix, provided as a '.csv' file, so all `nan` values are replaced
+    with the average of their neighbours."""
     # Load matrix
     in_matrix = np.loadtxt(csv_path, delimiter=",")
     # Interpolate with chosen method
