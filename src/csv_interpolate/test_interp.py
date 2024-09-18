88b118a5ce56357db84d3a802a0321bfe9e2c1ff
James Ashford <ashfordj@rocketmail.com>
2024-09-18 15:21:02 +0100

    Cleaned up tests

--- src/csv_interpolate/test_interp.py
+++ src/csv_interpolate/test_interp.py
@@ -2,20 +2,15 @@ from .interp_methods import interpolate_at_nans, interpolate_convolve
 import numpy as np
 
 
-DATA_DIR = "../interpolation/example_data/"
-IN_MATRIX = np.loadtxt(f"{DATA_DIR}input_test_data.csv", delimiter=",")
-SAMPLE_OUT_MATRIX = np.loadtxt(f"{DATA_DIR}/interpolated_test_data.csv", delimiter=",")
+IN_MATRIX = np.loadtxt("rsc/input_test_data.csv", delimiter=",")
+OUT_MATRIX = np.loadtxt("rsc/interpolated_test_data.csv", delimiter=",")
 
 
-# Acceptance tests: do each of these methods complete the sample data?
 def test_iterated_interp_correct():
     out_matrix = interpolate_at_nans(IN_MATRIX)
-    assert np.isclose(out_matrix, SAMPLE_OUT_MATRIX).all()
+    assert np.isclose(out_matrix, OUT_MATRIX).all()
 
 
 def test_conv_interp_correct():
     out_matrix = interpolate_convolve(IN_MATRIX)
-    assert np.isclose(out_matrix, SAMPLE_OUT_MATRIX).all()
-
-
-# Unit testing:
+    assert np.isclose(out_matrix, OUT_MATRIX).all()
